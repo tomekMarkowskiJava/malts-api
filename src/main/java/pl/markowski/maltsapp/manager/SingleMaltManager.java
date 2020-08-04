35de33c17d9fe63e6405052cc32599dd42caa83c
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-08-04 21:41:26 +0200

    Refactor

--- src/main/java/pl/markowski/maltsapp/manager/SingleMaltManager.java
+++ src/main/java/pl/markowski/maltsapp/manager/SingleMaltManager.java
@@ -1,8 +1,6 @@
 package pl.markowski.maltsapp.manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import pl.markowski.maltsapp.dao.SingleMaltRepo;
 import pl.markowski.maltsapp.dao.entity.SingleMalt;
