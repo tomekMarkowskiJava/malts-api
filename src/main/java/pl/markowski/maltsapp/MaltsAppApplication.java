aa8109fe1080b4593cf1c550ce0b575743593157
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-08-04 12:50:52 +0200

    Initial commit

---
+++ src/main/java/pl/markowski/maltsapp/MaltsAppApplication.java
@@ -0,0 +1,13 @@
+package pl.markowski.maltsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaltsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaltsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/markowski/maltsapp/MaltsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.markowski.maltsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaltsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
